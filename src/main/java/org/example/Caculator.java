3878ae72a61b5d93bc93d49a5bf0d50f4a8b3a21
Nguyen Manh Cuong <manhcuong@example.com>
2025-04-08 23:03:28 +0700

    first commit

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Caculator implements Icaulator{
+    public Integer sum(Integer a, Integer b){
+        return 0;
+    }
+    public Integer sub(Integer a, Integer b){
+        return 0;
+    }
+}
